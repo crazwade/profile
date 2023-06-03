4c759510a51173db3d4cffbf714cd19781f6915c
crazwade <jackywu526@gmail.com>
2023-06-03 16:42:34 +0800

    feat: install element-plu

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
@@ -9,5 +11,6 @@ const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
+app.use(ElementPlus);
 
 app.mount('#app');
