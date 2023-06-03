d944b5a5542fc363682c3e2d30219232769e0683
crazwade <jackywu526@gmail.com>
2023-06-03 15:55:25 +0800

    feat: install element-plusicon

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.mount('#app');
