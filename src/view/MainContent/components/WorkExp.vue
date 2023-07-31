36ef0e7784f722db79955ebebc4253bebcadfee5
jacky <jackywu526@gmail.com>
2023-07-31 18:09:07 +0800

    feat: 針對 i18n 處理 Array 進行調整

--- src/view/MainContent/components/WorkExp.vue
+++ src/view/MainContent/components/WorkExp.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-    Work Exp.
+    <div v-for="(item, index) in workExpItem" :key="index">
+      {{ item.company }}
+      <div>
+        {{ item.time }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang='ts'>
+import { useI18n } from 'vue-i18n';
+
+const { tm } = useI18n();
+
+type WorkExpType = {
+  company: string;
+  time: string;
+  title: string;
+  skill: string[];
+  project: {
+    name: string;
+    skill: string[];
+  }[];
+  content: string[];
+}
+
+const workExpItem = tm('experience') as WorkExpType[];
 </script>
 
 <style lang="scss" scoped>
-
 </style>
+
+
+// i18n
+{
+  "userTypes": ["普通用戶", "管理員", "訪客"],
+}
