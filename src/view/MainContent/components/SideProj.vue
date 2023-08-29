2ad5e07e5d976466685b5c19216dae921ee534d2
jacky <jackywu526@gmail.com>
2023-08-29 10:38:51 +0800

    style: side project 改成 text-blue-500 讓可點擊效果變明顯

--- src/view/MainContent/components/SideProj.vue
+++ src/view/MainContent/components/SideProj.vue
@@ -7,12 +7,12 @@
       <div class="inline">
         <span
           v-if="item.link"
-          class="font-semibold text-lg cursor-pointer hover:text-blue-600 hover:underline"
+          class="font-semibold text-lg text-blue-500 cursor-pointer hover:text-blue-500 hover:underline"
           @click="gotoDemo(item.link)"
         >
           {{ item.title }}
         </span>
-        <span v-else class="font-semibold text-lg">{{ item.title }}</span>
+        <span v-else class="font-semibold text-lg text-blue-500">{{ item.title }}</span>
         <span class="text-gray-600"> - </span>
         <span class="text-gray-600 text-sm">{{ item.detail }}</span>
       </div>
