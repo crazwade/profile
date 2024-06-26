e9dcddab76e9fc4d8d1c2810a53d8c26d4fc20f0
jacky <jackywu526@gmail.com>
2024-06-26 20:06:31 +0800

    feat(ContentWrapper.vue): 製作帶有標題的container

---
+++ src/components/ContentWrapper.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+defineProps<{
+  label: string
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col mt-5" >
+    <div class="text-[#F0F8FF] font-semibold text-lg bg-[#495F98] py-1 pl-4 mb-[0.6rem] flex justify-start items-center">
+      {{ label }}
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
