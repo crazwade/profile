72741fed48caae99b4989fe9a0be64289b828200
jacky <jackywu526@gmail.com>
2024-06-26 13:28:46 +0800

    feat(deploy.yml): 添加 自動部署 CI

--- src/view/MainContent/components/MyInfo.vue
+++ src/view/MainContent/components/MyInfo.vue
@@ -2,23 +2,22 @@
   <div class="text-black">
     <div class="mb-2 flex flex-wrap justify-between w-full">
       <div class="flex flex-col justify-start">
-        <div class=" text-5xl mt-2 font-semibold text-black">{{ $t('info.name') }}</div>
-        <div class=" text-sm my-4">{{ $t('info.degree') }}</div>
-        <div class=" border-t-2 border-gray-400 w-[85%]"></div>
+        <div class="text-5xl mt-2 font-semibold">{{ $t('info.name') }}</div>
+        <div class="text-sm my-4">{{ $t('info.degree') }}</div>
+        <div class="border-t-2 border-gray-400 w-[85%]"></div>
       </div>
       <div class="flex flex-wrap">
         <div
-          v-for="(item, index) in socialItem"
+          v-for="(item, index) in socialItems"
           :key="index"
           class="my-2 mr-2 icon-link flex items-center hover:text-blue-700 hover:font-bold"
         >
           <a
-            :href="item.link"
+            :href="item.name === 'email' ? `mailto:${item.link}` : item.link"
             target="_blank"
             class="mx-1 flex justify-center items-center"
           >
-            <i v-if="item.name === 'email'" class="far fa-envelope text-2xl"></i>
-            <i v-else :class="`fab fa-${item.name} text-2xl`"></i>
+            <i :class="item.name === 'email' ? 'far fa-envelope text-2xl' : `fab fa-${item.name} text-2xl`"></i>
           </a>
           <a
             :href="item.name === 'email' ? `mailto:${item.link}` : item.link"
@@ -34,11 +33,11 @@
       <el-col :span="24">
         <div>
           <span
-            v-for="(item, index) in tm('skills')"
+            v-for="(skill, index) in skills"
             :key="index"
             class="text-stone-600 bg-gray-100 text-sm rounded m-2 p-2 inline-block"
           >
-            {{ item }}
+            {{ skill }}
           </span>
         </div>
       </el-col>
@@ -57,7 +56,8 @@ type SocialObj = {
 
 const { tm } = useI18n();
 
-const socialItem = tm('social') as SocialObj[];
+const socialItems = tm('social') as SocialObj[];
+const skills = tm('skills') as string[];
 
 </script>
 
